9c0af7d25036aa7d251f45b96329b75d9309fcfd
anchal275 <graphicology27@gmail.com>
2022-07-24 02:23:04 +0530

    added

--- components/navbar.js
+++ components/navbar.js
@@ -9,27 +9,27 @@ let navbar = ()=>{
             <a href="#!">Solutions</a><span><i class="fa-solid fa-caret-down"></i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/></svg></span>
             <ul class="nav-dropdown">
               <li>
-                <a href="#!">Why Sendinblue?</a>
+                <a href="#">Why Sendinblue?</a>
               </li>
               <li>
-                <a href="#!">Enterprise</a>
+                <a href="#">Enterprise</a>
               </li>
             </ul>
           </li>
       <li>
-        <a href="#!">Pricing</a>
+        <a href="Pricing Page.html">Pricing</a>
       </li>
       <li>
-        <a href="#!">Features</a>
+        <a href="features.html">Features</a>
       </li>
       <li>
         <a href="#!">Resources</a><span><i class="fa-solid fa-caret-down"></i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/></svg></span>
         <ul class="nav-dropdown">
           <li>
-            <a href="#!">Email Marketing</a>
+            <a href="casestudy.html">Case Studies</a>
           </li>
           <li>
-            <a href="#!">Emsil API</a>
+            <a href="#!">Product updates</a>
           </li>
           <li>
             <a href="#!">SMS Marketing</a>
@@ -37,13 +37,13 @@ let navbar = ()=>{
         </ul>
       </li>
       <li>
-        <a href="#!">Blog</a>
+        <a href="blog.html">Blog</a>
       </li>
     </ul>
   </nav>
   <div>
-    <a id="login" href="#">Log In</a>
-    <button id="btn">Sign Up Free</button>
+    <a id="login" href="login.html">Log In</a>
+    <button id="btn"><a href="signup.html">Sign Up Free</a></button>
   </div>`
 }
 
